5e1fc3819e71e4ef05f3151794cdda76e502bc50
Zakiy <zakiy@icehousecorp.com>
2014-03-20 17:03:39 +0700

    Added new project

---
+++ VuforiaAR/app/src/main/java/com/example/app/LandingActivity.java
@@ -0,0 +1,28 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        Button imageTargetButton = (Button) findViewById(R.id.button_image_target);
+
+        imageTargetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingActivity.this, );
+                startActivity(intent);
+            }
+        });
+    }
+
+}
